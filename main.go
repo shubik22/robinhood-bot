1493d5093a56450d70c11b66f52e01b84bf9fb9a
Samuel Sweeney <sams@uber.com>
2016-08-01 14:39:55 -0700

    Revert "Actually don't ping self"
    This reverts commit 846ed7d28f52dac2b944312df3e73e973a0abaab.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
@@ -23,6 +24,7 @@ const (
 
 func main() {
 	go setupDummyWebserver()
+	go runPingLoop()
 
 	godotenv.Load()
 	twitterApi := getTwitterApi()
@@ -61,6 +63,25 @@ func setupDummyWebserver() {
 	})
 	port := os.Getenv("PORT")
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func runPingLoop() {
+	for {
+		pingSelf()
+		seconds := rand.Intn(300)
+		ticker := time.NewTicker(time.Duration(seconds) * time.Second)
+		<-ticker.C
+	}
+}
+
+func pingSelf() {
+	resp, err := http.Get("http://robinhood-bot.herokuapp.com")
+	if err != nil {
+		log.Printf("Error pinging self: %v", err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Printf("Pinged self: %v", body)
 }
 
 func handleMention(t *anaconda.Tweet, twitterApi *anaconda.TwitterApi, rhClient *robinhood.Client) {
